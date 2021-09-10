a4051190b3da5582a2f776401af7c71a5bad608b
DiamondLionLV <DiamondLion@ravine.com>
2021-09-10 23:07:52 +0300

    chengi

--- src/bot.js
+++ src/bot.js
@@ -1,30 +1,9 @@
 require("dotenv").config();
-const express = require("express")
-const app = express()
 
-
-app.get("/",(req, res) => {
-    res.send("Ready!")
-  })
-  
-  app.listen(3000, () => {
-    console.log("Up!")
-})
-
-
-const { Client, Intents, Permissions, Message, User, Reaction } = require('discord.js');
+const { Client, Intents, Permissions } = require('discord.js');
 const client = new Client({ partials: ["MESSAGE", "USER", "REACTION"], intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
-const Discord = require("discord.js");
-const enmap = require('enmap');
 
 const prefix = process.env.PREFIX;
-
-const settings = new enmap({
-    name: "settings",
-    autoFetch: true,
-    cloneLevel: "deep",
-    fetchAll: true
-});
 
 client.on('ready', () => {
     console.log(`${client.user.tag} has logged in.`);
