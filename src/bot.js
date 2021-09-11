461d73284e50fdfa1d9b047ec76f1ed49a4aa379
DiamondLionLV <DiamondLion@ravine.com>
2021-09-11 03:37:55 +0300

    sataisīju embed

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-const { Client, Intents, Permissions } = require('discord.js');
+const { Client, Intents, Permissions, Discord, MessageEmbed } = require('discord.js');
 const client = new Client({ partials: ["MESSAGE", "USER", "REACTION"], intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
 
 const prefix = process.env.PREFIX;
@@ -8,6 +8,7 @@ const prefix = process.env.PREFIX;
 client.on('ready', () => {
     console.log(`${client.user.tag} has logged in.`);
 });
+
 
 client.on('message', async (message) => {
     if (message.content.startsWith(prefix + 'say')) {
@@ -22,6 +23,26 @@ client.on('message', async (message) => {
     } else {
         return;
     }
+});
+
+client.on('message', async (message) => {
+    if (message.content.startsWith(prefix + 'embed')) {
+        if (message.author.bot) return;
+        message.delete();
+        const kanalaid = "885870398682583063"
+        const embedi = new MessageEmbed()
+            .setTitle("mājas darbi")
+            .setDescription("Šeit sūtam mājas darbus, lai varētu špikot viens no otra :smile:")
+            .setFooter("~Iedzīvotājs")
+            .setColor("#009602");
+        
+        message.channel.send({ embeds: [new MessageEmbed().setTitle("mājasdarbi").setDescription("Šeit sūtam mājas darbus, lai varētu špikot viens no otra :smile:").setFooter("~Iedzīvotājs").setColor("#009602")] })
+    }
+
+    process.on('unhandledRejection', error => {
+        console.error('Unhandled promise rejection:', error);
+    });
+
 });
 
 client.login(process.env.DISCORDJS_BOT_TOKEN);
