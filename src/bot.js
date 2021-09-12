c22c5917053ec823edbf9b2e1c217606c42883e4
DiamondLionLV <DiamondLion@ravine.com>
2021-09-12 21:26:59 +0300

    Pievienoju ?voice pie help

--- src/bot.js
+++ src/bot.js
@@ -142,6 +142,7 @@ client.on('messageCreate', async (message) => {
             .addField("**Komandu prefix:**", "?")
             .addField("**Informācija par šo discord serveri:**", "?info")
             .addField("**Ieteikt/piedāvāt kādu ideju vai jebko citu kopīgam balsojumam:**", "?ieteikt teksts")
+            .addField("**Izveidot jaunu voice kanālu:**", "?voice nosaukums")
             .setFooter("~Iedzīvotājs")
             .setTimestamp()
         ]})
